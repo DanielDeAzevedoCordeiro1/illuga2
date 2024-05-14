ae3fd027974bce8852e4adf76f82cd996267cffe
unknown <Daniel>
2024-05-13 22:50:54 -0300

    Subindo rotas

--- callback.js
+++
@@ -1,7 +0,0 @@
-const array = [1,2,34,556,66]
-
-const mostradados = array.forEach((item) => {
-    return item
-})
-
-console.log(mostradados)
--- main.js
+++ main.js
@@ -1,58 +1,33 @@
-function Logar(){
-    
-    let email = document.querySelector("#email").value;
-    let senha = document.querySelector("#senha").value;
-    let banco = JSON.parse(localStorage.getItem("banco")); 
-    
-    let UsuarioEncontrado = false;
-    
-    if(banco){
-        for (let v = 0 ; v < banco.length; v++){
-                if (email === banco[v].email && senha === banco[v].senha) {
-                    alert("Achou!");
-                    UsuarioEncontrado = true;
-                    break;
-                }
-            }
-            
-            if (UsuarioEncontrado) {
-                window.open("index.html");
-            }else{
-                alert("Cadastre-se!");
-                window.open("cadastro.html");
-            }
-        }
- }
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 6065;
 
-function Cadastrar(){
-    
-    let nome = document.querySelector("#nome").value;
-    let senha = document.querySelector("#senha1").value;
-    let senha2 = document.querySelector("#senha2").value;
-    let email = document.querySelector("#email").value;
-    let cep = document.querySelector("#cep").value;
-    let rua = document.querySelector("#rua").value;
-    let numero = document.querySelector("#numero").value;
-    let cidade = document.querySelector("#cidade").value;
-    let estado = document.querySelector("#estado").value;
-    let complemento = document.querySelector("#complemento").value;
+// Define uma rota para a página inicial
+app.get('/', (req, res) => {
+    res.json({ 'Status': 'OK' });
+});
 
-    const banco = JSON.parse(localStorage.getItem("banco")) || []
-    let provaSenha = false;
+// Define a pasta onde os arquivos estáticos estão localizados
+const pastaEstática = path.join(__dirname, 'public');
 
-    if (senha == senha2 ) {
-        provaSenha = true;
-    }else{
-        alert("Coloque sua senha igual a primeira!")
-        return;
-    }
-    if (provaSenha) {
-        let novoUsuario = {nome:nome,senha:senha,email:email,estado:estado,cidade:cidade,cep:cep,rua:rua,complemento:complemento,numero:numero}
-        banco.push(novoUsuario);
-        localStorage.setItem("banco",JSON.stringify(banco));
-        alert("cadastrado!");
-        window.open("login.html");
-    }else{
-        alert("Cadastro incompleto!");
-        }
-    }
+// Define a rota para servir arquivos estáticos a partir da pasta definida
+app.use(express.static(pastaEstática));
+
+// Define uma rota para a página index.html
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(pastaEstática, 'index.html'));
+});
+
+//Rota para o cadastro
+app.get('/index/cadastro', (req,res) => {
+    res.sendFile(path.join(pastaEstática, 'cadastro.html'))
+})
+
+//Rota para login
+app.get('/index/cadastro/login', (req,res) => res.sendFile(path.join(pastaEstática, 'login.html')))
+
+// Inicia o servidor na porta especificada
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
